45c58bae37931ccd031bc06b61fe448886ca5204
TungNguyen6688 <nguyenxuantung1221@gmail.com>
2019-08-07 15:57:04 +0700

    html/css home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 ﻿$(function () {
+      // init Isotope
+      var $listitem = $('.listitem').isotope({
+            // options...
+            itemSelector: '.item',
+            layoutMode: 'masonry'
+      });
+      // layout Isotope after each image loads
+      $listitem.imagesLoaded().progress(function () {
+            $listitem.isotope('layout');
+      });
+
+
+      $('.bottommenu .titlemenu a').click(function(){
+            var data = $(this).data('filter');
+            $listitem.isotope({ filter: data });            
+            return false;
+      });
+
       new WOW().init();
+
 })
